ec793215c07e2d4feeda007d77bc69e077025549
emircanaydin <emircanaydin@menulux.com>
2021-11-03 00:51:44 +0300

    Initial Commit

---
+++ OMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OMDB
+//
+//  Created by Emircan Aydın on 3.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
